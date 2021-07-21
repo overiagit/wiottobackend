d8f689737d5e45df3c0dd0c515c09978a14664ec
kOlexyuk <k.Olexyuk@gmail.com>
2021-07-21 13:44:19 +0300

    set index

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -153,6 +153,8 @@ class SiteController extends Controller
      */
     public function actionSignup()
     {
+        return $this->goHome();
+
         $model = new SignupForm();
         if ($model->load(Yii::$app->request->post()) && $model->signup()) {
             Yii::$app->session->setFlash('success', 'Thank you for registration. Please check your inbox for verification email.');
