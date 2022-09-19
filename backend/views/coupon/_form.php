ef43ae7f19722734191dd8246ec1e56213a9edad
kostya <k.olexyuk@gmail.com>
2022-09-19 14:25:28 +0300

    coupons

--- backend/views/coupon/_form.php
+++ backend/views/coupon/_form.php
@@ -12,18 +12,28 @@ use kartik\widgets\DatePicker;
 <div class="coupon-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
+    <div class="form-group row">
+        <div class="col col-md-10 ">
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'note')->textInput(['maxlength' => true]) ?>
-
+        </div>
+    </div>
+    <div class="form-group row">
+        <div class="col col-md-10 ">
+    <?= $form->field($model, 'note')->textarea(['maxlength' => true]) ?>
+        </div>
+    </div>
+    <div class="form-group row">
+        <div class="col col-md-2 ">
     <?= $form->field($model, 'percent')->textInput(['type'=>'number', 'step'=>1]) ?>
-
+        </div>
+    </div>
+    <div class="form-group row">
+        <div class="col col-md-2 ">
     <?= $form->field($model, 'date_from')->widget(
         DatePicker::className(),
         [
             'type' => DatePicker::TYPE_INPUT,
-            'options' => ['placeholder' => 'Select date ...'],
+            'options' => ['placeholder' => 'Select date ...','format' => 'yyyy-mm-dd',],
 //            'convertFormat' => true,
             'pluginOptions' => [
                 'format' => 'yyyy-mm-dd',
@@ -33,12 +43,15 @@ use kartik\widgets\DatePicker;
             ]
         ]
     ); ?>
-
+        </div>
+    </div>
+    <div class="form-group row">
+        <div class="col col-md-2 ">
     <?= $form->field($model, 'date_to')->widget(
         DatePicker::className(),
         [
             'type' => DatePicker::TYPE_INPUT,
-           'options' => ['placeholder' => 'Select date ...'],
+           'options' => ['placeholder' => 'Select date ...','format' => 'yyyy-mm-dd',],
 //            'convertFormat' => true,
             'pluginOptions' => [
                 'format' => 'yyyy-mm-dd',
@@ -49,8 +62,13 @@ use kartik\widgets\DatePicker;
         ]
     );
  ?>
-
+        </div>
+    </div>
+    <div class="form-group row">
+        <div class="col col-md-1 ">
     <?= $form->field($model, 'active')->checkbox() ?>
+        </div>
+    </div>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
