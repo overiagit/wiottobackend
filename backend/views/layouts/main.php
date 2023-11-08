9bdf802e964a8c64a93459e6c08979e4ba9b7254
kostya <k.olexyuk@gmail.com>
2023-11-08 11:37:20 +0200

    add option-group

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -47,6 +47,7 @@ AppAsset::register($this);
         $menuItems[] = ['label' => 'Plan you trip', 'url' => ['/plan-trip']];
         $menuItems[] = ['label' => 'Coupon', 'url' => ['/coupon']];
         $menuItems[] = ['label' => 'NotLinled', 'url' => ['/linkroom']];
+        $menuItems[] = ['label' => 'Features group', 'url' => ['/option-group']];
         $menuItems[] = ['label' => 'Features', 'url' => ['/option']];
         $menuItems[] = '<li>'
             . Html::beginForm(['/site/logout'], 'post', ['class' => 'form-inline'])
